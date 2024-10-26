7d0ed98ffd25e4c7bc490256b88db0bcb757fcba
harshitkaushik444555 <harshitkaushik444555@gmail.com>
2024-10-26 20:31:04 +0530

    Update main.tf

--- Terraform-Projects/EC2-Instance-Creation-Using-Modules/main.tf
+++ Terraform-Projects/EC2-Instance-Creation-Using-Modules/main.tf
@@ -6,5 +6,7 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-053b0d53c279acc90" # replace this
   instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  tags = {
+    Name = var.resource_name
+  }
 }
