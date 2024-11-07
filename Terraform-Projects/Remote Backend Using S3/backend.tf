e805fdf1e0a107c76151fd97e2faa84bbd1509e0
harshitkaushik444555 <harshitkaushik444555@gmail.com>
2024-11-07 14:20:39 +0530

    Create backend.tf

---
+++ Terraform-Projects/Remote Backend Using S3/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "harshit-terraform-state-bucket" # first create this bucket on aws
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "harshit-dynamodb-table" //dynamo table name
+  }
+}
